010cf3018470a9f0215bbbc2766f35b187562934
vishalibitwar <vish31ibitwar@gmail.com>
2020-03-08 23:32:15 +0530

    first commit

---
+++ src/app.js
@@ -0,0 +1,98 @@
+import {
+  http
+} from './http';
+import {
+  ui
+} from './ui';
+
+
+
+document.addEventListener('DOMContentLoaded', getPosts);
+document.querySelector('.post-submit').addEventListener('click', submitPost);
+document.querySelector('#posts').addEventListener('click', deletePost);
+document.querySelector('#posts').addEventListener('click', enableEdit);
+document.querySelector('.card-form').addEventListener('click', cancelEdit);
+
+function getPosts() {
+  http.get('http://localhost:3000/posts')
+    .then(data => ui.showPosts(data))
+    .catch(err => console.log(err));
+}
+
+
+function submitPost() {
+
+  const title = document.querySelector('#title').value;
+  const body = document.querySelector('#body').value;
+  const id = document.querySelector('#id').value;
+
+  const data = {
+    title,
+    body
+  }
+
+  if (title === '' || body === '') {
+    ui.showAlert('please Fill all the Fields', 'alert alert-danger');
+  } else {
+
+    if (id == '') {
+      http.post('http://localhost:3000/posts', data)
+        .then(data => {
+          ui.showAlert('Post added', 'alert alert-success');
+          ui.clearFields();
+          getPosts();
+        })
+        .catch(err => console.log(err));
+    } else {
+      http.put(`http://localhost:3000/posts/${id}`, data)
+        .then(data => {
+          ui.showAlert('Post added', 'alert alert-success');
+          ui.changeFormState('add');
+          getPosts();
+        })
+        .catch(err => console.log(err));
+    }
+  }
+}
+
+function deletePost(e) {
+
+  if (e.target.parentElement.classList.contains('delete')) {
+    const id = e.target.parentElement.dataset.id;
+    if (confirm('Are you sure ?')) {
+      http.delete(`http://localhost:3000/posts/${id}`)
+        .then(data => {
+          ui.showAlert('Post Deleted ', 'alert alert-danger');
+          getPosts();
+        })
+        .catch(err => console.log(err));
+    }
+  }
+  e.preventDefault();
+}
+
+
+function enableEdit(e) {
+  if (e.target.parentElement.classList.contains('edit')) {
+    const id = e.target.parentElement.dataset.id;
+    const title = e.target.parentElement.previousElementSibling.previousElementSibling.textContent;
+    const body = e.target.parentElement.previousElementSibling.textContent;
+    const data = {
+      id,
+      title,
+      body
+    }
+    ui.fillForm(data);
+
+  }
+  e.preventDefault();
+}
+
+function cancelEdit(e) {
+  if (e.target.classList.contains('post-cancel')) {
+    ui.changeFormState('add');
+  }
+
+  e.preventDefault();
+
+}
---
+++ src/ui.js
@@ -0,0 +1,98 @@
+class UI {
+
+  constructor() {
+    this.post = document.querySelector('#posts');
+    this.titleInput = document.querySelector('#title');
+    this.bodyInput = document.querySelector('#body');
+    this.idInput = document.querySelector('#id');
+    this.postSubmit = document.querySelector('.post-submit');
+    this.forState = 'add';
+
+  }
+
+  showPosts(posts) {
+    let output = '';
+    posts.forEach(post => {
+      output += `
+     <div class="card cardbox  mb-3">
+      <div class="card-body text-center">
+        <h4 class="card-title">${post.title}</h4>
+        <p class="card-text h2 my-1 ">${post.body}</p>
+        <a href="#" class="edit card-link " data-id="${post.id}">
+         <i class="fa fa-pencil"></i>
+         </a>
+        <a href="#" class="delete card-link " data-id="${post.id}">
+         <i class="fa fa-remove"></i>
+         </a>
+      </div>
+    </div>
+        `;
+    });
+    this.post.innerHTML = output;
+  }
+
+  showAlert(message, className) {
+    this.clearAlert();
+
+    const div = document.createElement('div');
+    div.className = className;
+    div.appendChild(document.createTextNode(message));
+    const container = document.querySelector('.postsContainer');
+    const posts = document.querySelector('#posts');
+    container.insertBefore(div, posts);
+    setTimeout(() => {
+      this.clearAlert();
+    }, 3000);
+
+
+  }
+
+  clearAlert() {
+    const currentAlert = document.querySelector('.alert');
+    if (currentAlert) {
+      currentAlert.remove();
+    }
+  }
+
+  clearFields() {
+    this.titleInput.value = '';
+    this.bodyInput.value = '';
+  }
+
+  fillForm(data) {
+    this.titleInput.value = data.title;
+    this.bodyInput.value = data.body;
+    this.idInput.value = data.id;
+    this.changeFormState('edit');
+  }
+
+  clearIdInput() {
+    this.idInput.value = '';
+  }
+
+  changeFormState(type) {
+    if (type == 'edit') {
+      this.postSubmit.textContent = 'Update Post';
+      this.postSubmit.className = 'post-Submit btn btn-primary btn-block ';
+      if (document.querySelector('.post-cancel')) {
+        document.querySelector('.post-cancel').remove();
+      }
+      const button = document.createElement('button');
+      button.className = 'post-cancel btn btn-light btn-block';
+      button.appendChild(document.createTextNode('Cancle Edit'));
+      const cardForm = document.querySelector('.card-form');
+      const formEnd = document.querySelector('.form-end');
+      cardForm.insertBefore(button, formEnd);
+    } else {
+      this.postSubmit.textContent = 'Post It';
+      this.postSubmit.className = 'post-Submit btn btn-info btn-block ';
+      if (document.querySelector('.post-cancel')) {
+        document.querySelector('.post-cancel').remove();
+      }
+      this.clearIdInput();
+      this.clearFields();
+    }
+  }
+}
+
+export const ui = new UI();
